815d23d28140b357f5cbff1755539612221f3298
meeman <meeman@PLAY-RISK-OF-RAIN-2.localdomain>
2025-12-15 04:27:06 +0000

    experimenting with material attributes

--- material.go
+++ material.go
@@ -2,7 +2,11 @@ package main
 
 type Material struct {
 	BasicItem
-	Rarity int
+	Rarity             int
+	DamageMultiplier   float64
+	WeightMultiplier   float64
+	HandlingMultiplier float64
+	HardnessMultiplier float64
 }
 
 // Material types
@@ -10,47 +14,75 @@ var Wood Material = Material{
 	BasicItem: BasicItem{ID: "00001",
 		Name:     "Wood",
 		MaxStack: 64},
-	Rarity: 0,
+	Rarity:             0,
+	DamageMultiplier:   1,
+	WeightMultiplier:   1,
+	HandlingMultiplier: 1,
+	HardnessMultiplier: 1,
 }
 
 var Stone Material = Material{
 	BasicItem: BasicItem{ID: "00002",
 		Name:     "Stone",
 		MaxStack: 64},
-	Rarity: 1,
+	Rarity:             1,
+	DamageMultiplier:   2,
+	WeightMultiplier:   2,
+	HandlingMultiplier: 2,
+	HardnessMultiplier: 2,
 }
 
 var Coal Material = Material{
 	BasicItem: BasicItem{ID: "00003",
 		Name:     "Coal",
 		MaxStack: 64},
-	Rarity: 1,
+	Rarity:             1,
+	DamageMultiplier:   0,
+	WeightMultiplier:   0,
+	HandlingMultiplier: 0,
+	HardnessMultiplier: 0,
 }
 
 var Iron Material = Material{
 	BasicItem: BasicItem{ID: "00004",
 		Name:     "Iron",
 		MaxStack: 64},
-	Rarity: 1,
+	Rarity:             1,
+	DamageMultiplier:   3,
+	WeightMultiplier:   3,
+	HandlingMultiplier: 3,
+	HardnessMultiplier: 3,
 }
 
 var Steel Material = Material{
 	BasicItem: BasicItem{ID: "00005",
 		Name:     "Steel",
 		MaxStack: 64},
-	Rarity: 2,
+	Rarity:             2,
+	DamageMultiplier:   4,
+	WeightMultiplier:   4,
+	HandlingMultiplier: 4,
+	HardnessMultiplier: 4,
 }
 
 var Mithril Material = Material{
 	BasicItem: BasicItem{ID: "00006",
 		Name:     "Mithril",
 		MaxStack: 16},
-	Rarity: 4,
+	Rarity:             4,
+	DamageMultiplier:   5,
+	WeightMultiplier:   5,
+	HandlingMultiplier: 5,
+	HardnessMultiplier: 5,
 }
 
 var DragonBones Material = Material{
 	BasicItem: BasicItem{ID: "00007",
 		Name:     "Dragon Bones",
 		MaxStack: 2},
-	Rarity: 5,
+	Rarity:             5,
+	DamageMultiplier:   6,
+	WeightMultiplier:   6,
+	HandlingMultiplier: 6,
+	HardnessMultiplier: 6,
 }
--- weapons.go
+++ weapons.go
@@ -4,7 +4,10 @@ import "fmt"
 
 type WeaponPart struct {
 	BasicItem
-	PartType string
+	PartType           string
+	DamageMultiplier   float64
+	HandlingMultiplier float64
+	WeightMultiplier   float64
 }
 
 type Weapon struct {
@@ -27,6 +30,7 @@ func (w Weapon) GetMaxStack() int {
 }
 
 // variable to ensure unique ID (only in 1 session)
+var HighestPartID int
 var HighestSwordID int
 
 // placeholder damage, speed, part and sword
@@ -35,12 +39,66 @@ var defaultSwordSpeed int = 1
 
 var defaultPart WeaponPart = WeaponPart{
 	BasicItem: BasicItem{
-		ID:       "0000010",
+		ID:       "P000001",
 		Name:     "BasicPart",
 		MaxStack: 32},
 	PartType: "Basic",
 }
 var defaultSword Item = CreateWeapon("Sword", defaultPart, defaultPart, defaultPart)
+
+func newPartID() string {
+	HighestPartID++
+	return fmt.Sprintf("P%.6d", HighestPartID)
+}
+
+func CreateWeaponPart(PartType string, Material Material) WeaponPart {
+	newPart := defaultPart
+	// these are all the same rn but later will have different attributes depending on type
+	if PartType == "Handle" {
+		newPart := WeaponPart{
+			BasicItem: BasicItem{
+				ID:       newPartID(),
+				Name:     Material.Name + " " + PartType,
+				MaxStack: 1,
+			},
+			PartType:           PartType,
+			DamageMultiplier:   Material.DamageMultiplier,
+			HandlingMultiplier: Material.HandlingMultiplier,
+			WeightMultiplier:   Material.WeightMultiplier,
+		}
+		return newPart
+
+	}
+	if PartType == "Guard" {
+		newPart := WeaponPart{
+			BasicItem: BasicItem{
+				ID:       newPartID(),
+				Name:     Material.Name + " " + PartType,
+				MaxStack: 1,
+			},
+			PartType:           PartType,
+			DamageMultiplier:   Material.DamageMultiplier,
+			HandlingMultiplier: Material.HandlingMultiplier,
+			WeightMultiplier:   Material.WeightMultiplier,
+		}
+		return newPart
+	}
+	if PartType == "Blade" {
+		newPart := WeaponPart{
+			BasicItem: BasicItem{
+				ID:       newPartID(),
+				Name:     Material.Name + " " + PartType,
+				MaxStack: 1,
+			},
+			PartType:           PartType,
+			DamageMultiplier:   Material.DamageMultiplier,
+			HandlingMultiplier: Material.HandlingMultiplier,
+			WeightMultiplier:   Material.WeightMultiplier,
+		}
+		return newPart
+	}
+	return newPart
+}
 
 func newSwordID() string {
 	HighestSwordID++
@@ -55,8 +113,8 @@ func CreateWeapon(WeaponType string, Part1, Part2, Part3 WeaponPart) Item {
 				BasicItem: BasicItem{ID: newSwordID(),
 					Name:     fmt.Sprintf("%v + %v + %v Sword", Part1.Name, Part2.Name, Part3.Name),
 					MaxStack: 1},
-				Damage: float64(defaultSwordDamage),
-				Speed:  float64(defaultSwordSpeed),
+				Damage: float64(defaultSwordDamage) * Part1.DamageMultiplier * Part2.DamageMultiplier * Part3.DamageMultiplier,
+				Speed:  (float64(defaultSwordSpeed) * Part1.HandlingMultiplier * Part2.HandlingMultiplier * Part3.HandlingMultiplier) / (Part1.WeightMultiplier + Part2.WeightMultiplier + Part2.WeightMultiplier),
 			},
 			Blade:  Part1,
 			Guard:  Part2,
